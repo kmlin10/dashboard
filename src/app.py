581b52e904c3cf388004a862968e738335c8d562
Melody Lin <melody@Melodys-MBP.attlocal.net>
2023-09-25 11:21:01 -0500

    add 09251120

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 app = Dash(__name__)
 server = app.server
 
-df = pd.read_csv('src/age_0921_for_interactivechart_new.csv')
+df = pd.read_csv('age_0921_for_interactivechart_new.csv')
 
 category_choices = df['Category'].sort_values().unique()
 term_choices = df['Term'].sort_values().unique()
